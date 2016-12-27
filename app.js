123716253127a128c7c9e6f7d609000344d14305
mariajcb <bogomazm@gmail.com>
2016-12-27 15:12:04 -0700

    fixing routes

--- app.js
+++ app.js
@@ -9,7 +9,14 @@ const app = express();
 const uuid = require('uuid');
 
 const config = require('./config');
-const webhook = require('./routes/webhook');
+// const webhook = require('./routes/webhook');
+
+var receivedAccountLink = require('./messagingEvents/accountLink')
+var receivedMessage = require('./messagingEvents/message/receivedMessage')
+var receivedDeliveryConfirmation = require('./messagingEvents/deliveryConfirmation')
+var receivedPostback = require('./messagingEvents/postback')
+var receivedMessageRead = require('./messagingEvents/messageRead')
+var receivedAuthentication = require('./messagingEvents/authentication')
 
 // Messenger API parameters
 if (!config.FB_PAGE_TOKEN) {
@@ -28,7 +35,7 @@ if (!config.SERVER_URL) { //used for ink to static files
     throw new Error('missing SERVER_URL');
 }
 if (!config.SENDGRID_API_KEY) { //sending email
-	throw new Error('missing SENGRID_API_KEY');
+	throw new Error('missing SENDGRID_API_KEY');
 }
 if (!config.EMAIL_FROM) { //sending email
 	throw new Error('missing EMAIL_FROM');
@@ -55,6 +62,64 @@ app.use(bodyParser.urlencoded({
 
 // Process application/json
 app.use(bodyParser.json())
+
+// for Facebook verification
+app.get('/webhook/', function (req, res) {
+	console.log("request");
+	if (req.query['hub.mode'] === 'subscribe' && req.query['hub.verify_token'] === config.FB_VERIFY_TOKEN) {
+		res.status(200).send(req.query['hub.challenge']);
+	} else {
+		console.error("Failed validation. Make sure the validation tokens match.");
+		res.sendStatus(403);
+	}
+})
+
+/*
+ * All callbacks for Messenger are POST-ed. They will be sent to the same
+ * webhook. Be sure to subscribe your app to your page to receive callbacks
+ * for your page.
+ * https://developers.facebook.com/docs/messenger-platform/product-overview/setup#subscribe_app
+ *
+ */
+app.post('/webhook/', function (req, res) {
+	var data = req.body;
+	console.log(JSON.stringify(data));
+
+
+
+	// Make sure this is a page subscription
+	if (data.object == 'page') {
+		// Iterate over each entry
+		// There may be multiple if batched
+		data.entry.forEach(function (pageEntry) {
+			var pageID = pageEntry.id;
+			var timeOfEvent = pageEntry.time;
+
+			// Iterate over each messaging event
+			pageEntry.messaging.forEach(function (messagingEvent) {
+				if (messagingEvent.optin) {
+					receivedAuthentication(messagingEvent);
+				} else if (messagingEvent.message) {
+					receivedMessage(messagingEvent);
+				} else if (messagingEvent.delivery) {
+					receivedDeliveryConfirmation(messagingEvent);
+				} else if (messagingEvent.postback) {
+					receivedPostback(messagingEvent);
+				} else if (messagingEvent.read) {
+					receivedMessageRead(messagingEvent);
+				} else if (messagingEvent.account_linking) {
+					receivedAccountLink(messagingEvent);
+				} else {
+					console.log("Webhook received unknown messagingEvent: ", messagingEvent);
+				}
+			});
+		});
+
+		// Assume all went well.
+		// You must send back a 200, within 20 seconds
+		res.sendStatus(200);
+	}
+});
 
 
 const apiAiService = apiai(config.API_AI_CLIENT_ACCESS_TOKEN, {
